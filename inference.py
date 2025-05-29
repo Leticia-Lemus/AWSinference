6e61c847cfcd10e191c58725b7afe87e687c2532
Leticia-Lemus <leticcia.lemux@gmail.com>
2025-05-29 12:40:37 -0600

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,11 +1,11 @@
-#import joblib
-import tensorflow as tf
-import numpy as np
+import joblib
+#import tensorflow as tf
+#import numpy as np
 
 # Datos fijos para prueba/deben coincidir con el modelo_diabetes.keras
-features = [[2.000000, 99.000000, 72.405184, 29.153420, 155.548223, 22.200000, 0.108000, 23.000000]]
+features = [[1, 45, 58, 11, 22, 0]]
 # Cargar el modelo
-modelo = joblib.load("modelo_diabetes.keras")
+modelo = joblib.load("pinguinos_mlp_model.pkl")
 # Hacer la predicción
 prediccion = modelo.predict(features)
 print("Predicción:", prediccion[0])
